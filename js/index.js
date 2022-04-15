e2bc134ad3c92f5cd1031cfef679d6ed7804b075
Andrew <mysecondmail0099@gmail.com>
2022-04-15 16:30:42 +0300

    desktop test version

---
+++ js/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+
+document.querySelectorAll('.title_wrap').forEach((el) => {
+  el.addEventListener('click', () => {
+    let content = el.nextElementSibling;
+    if (content.style.maxHeight) {
+      document.querySelectorAll('.content_wrap').forEach((el) => el.style.maxHeight = null);
+    } else {
+      document.querySelectorAll('.content_wrap').forEach((el) => el.style.maxHeight = null);
+      content.style.maxHeight = content.scrollHeight + 'px'
+    }
+    document.querySelectorAll('.fa-caret-right').forEach((el) => {
+      el.classList.toggle('rotate')
+    })
+  })
+})
+
