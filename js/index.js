a972a5e90d898140a7636d3a899d47445c598753
Andrew <dd@rh-s.com>
2022-04-18 14:15:41 +0300

    finish mobile

--- js/index.js
+++ js/index.js
@@ -10,10 +10,20 @@ document.querySelectorAll('.title_wrap').forEach((el) => {
       document.querySelectorAll('.content_wrap').forEach((el) => el.style.maxHeight = null);
       content.style.maxHeight = content.scrollHeight + 'px'
     }
-    // document.querySelectorAll('.fa-caret-right').forEach((el) => {
-    //   el.classList.toggle('rotate')
-    // })
   })
 })
 
 
+
+// burger 
+
+let burger = document.querySelector('.burger');
+
+if (burger) {
+  const burgerMenu = document.querySelector('.burger_content');
+  burger.addEventListener('click', () => {
+    burgerMenu.classList.toggle('_active');
+    burger.classList.toggle('_active');
+    document.body.classList.toggle('_lock');
+  })
+}
